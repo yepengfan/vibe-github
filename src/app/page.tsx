195db3e4d13825976f0c7807d7fbcc1c32abcd10
Ted Fan <ted.fan@flightcentre.com>
2025-09-22 07:38:22 +1000

    02: database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-import { Button } from "@/components/ui/button";
-
 const Page = () => {
-  return (
-    <div>
-      <Button>Click Me</Button>
-    </div>
-  );
+  return <div>Hello World!</div>;
 };
 
 export default Page;
