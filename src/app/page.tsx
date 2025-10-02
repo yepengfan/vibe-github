086d11d4d193ccb08dd5451b4b5455e360618502
Ted Fan <ted.fan@flightcentre.com>
2025-10-03 07:41:37 +1000

    03: trpc-setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,37 @@
-const Page = () => {
-  return <div>Hello World!</div>;
+// "use client";
+
+// import { useTRPC } from "@/trpc/client";
+// import { useQuery } from "@tanstack/react-query";
+
+// const Page = () => {
+//   const trpc = useTRPC();
+//   const { data } = useQuery(
+//     trpc.createAI.queryOptions({ text: "hello world!" })
+//   );
+
+//   return <div>{JSON.stringify(data)}</div>;
+// };
+
+// export default Page;
+
+import { trpc, getQueryClient } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Client } from "./client";
+import { Suspense } from "react";
+
+const Page = async () => {
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(
+    trpc.createAI.queryOptions({ text: "prefetch!" })
+  );
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Client />
+      </Suspense>
+    </HydrationBoundary>
+  );
 };
 
 export default Page;
